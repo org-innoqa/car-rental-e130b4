9b388908d4329ebcf87694ea5e434f1dc545fa9a
ek-innoqa <ek@inno.qa>
2026-07-22 11:10:38 +0300

    AI: Update project files

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,7 +4,6 @@ interface ImportMetaEnv {
   readonly VITE_POSTGREST_URL: string;
   readonly VITE_DB_SCHEMA: string;
   readonly VITE_DB_ANON_TOKEN?: string;
-  readonly VITE_GOOGLE_MAPS_API_KEY?: string;
 }
 
 interface ImportMeta {
